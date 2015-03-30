e0aca05aed5594c33779efb2f2aba7fd697c8919
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-03-30 12:16:18 +0300

    referee

--- verification/referee.py
+++ verification/referee.py
@@ -29,8 +29,6 @@ checkio.referee.cover_codes
 from checkio.signals import ON_CONNECT
 from checkio import api
 from checkio.referees.io import CheckiOReferee
-from checkio.referees import cover_codes
-from checkio.referees import checkers
 
 from tests import TESTS
 
@@ -39,11 +37,7 @@ api.add_listener(
     CheckiOReferee(
         tests=TESTS,
         cover_code={
-            'python-27': cover_codes.unwrap_args,  # or None
-            'python-3': cover_codes.unwrap_args
+            'python-27': "cover=lambda f,d: return f(str(d))",
+            'python-3': None
         },
-        # checker=None,  # checkers.float.comparison(2)
-        # add_allowed_modules=[],
-        # add_close_builtins=[],
-        # remove_allowed_modules=[]
     ).on_ready)
